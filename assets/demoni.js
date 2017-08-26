8e9205ca6e07177ec24bdc5b5f84a955047a5cc9
Atte Lainejoki <atte.lainejoki@gmail.com>
2017-08-26 11:35:09 +0300

    Remember the brutalist choices with sessionStorage

--- assets/demoni.js
+++ assets/demoni.js
@@ -1,11 +1,18 @@
+// ADD BRUTALISM
 var body = document.body;
 var html = document.documentElement;
 var logo = document.getElementById('footer-logo');
 
 logo.onclick = function() {
   if (body.className.search(/\bbrutal\b/ig) !== -1) {
+    sessionStorage.removeItem('brutal');
     body.classList.remove('brutal');
   } else {
+    sessionStorage.setItem('brutal', 'true');
     body.className += 'brutal';
   }
+}
+
+if (sessionStorage.getItem('brutal') && body.className.search(/\bbrutal\b/ig) == -1) {
+  body.className += 'brutal';
 }
